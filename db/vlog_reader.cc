528452199453bd1affc89bf99a98550397652abf
guo-shaoge <shaoge1994@163.com>
2018-08-10 23:43:09 +0800

    some bug. FIXME is added

--- db/vlog_reader.cc
+++ db/vlog_reader.cc
@@ -20,7 +20,7 @@ VReader::VReader(SequentialFile* file,  bool checksum,
     : file_(file),
       reporter_ (NULL),
       checksum_(checksum),
-      backing_store_(new char[kBlockSize]),//一次从磁盘读kblocksize，多余的做缓存以便下次读
+      backing_store_(new char[kBlockSize]),
       buffer_(),
       eof_(false){
     if(initial_offset > 0)
@@ -32,7 +32,7 @@ VReader::VReader(SequentialFile* file, Reporter* reporter, bool checksum,
     : file_(file),
       reporter_ (reporter),
       checksum_(checksum),
-      backing_store_(new char[kBlockSize]),//一次从磁盘读kblocksize，多余的做缓存以便下次读
+      backing_store_(new char[kBlockSize]),
       buffer_(),
       eof_(false){
     if(initial_offset > 0)
@@ -45,7 +45,7 @@ VReader::~VReader() {
 }
 
 bool VReader::SkipToPos(size_t pos) {
-  if (pos > 0) {//跳到距file文件头偏移pos的地方
+  if (pos > 0) {
       Status skip_status = file_->SkipFromHead(pos);
     if (!skip_status.ok()) {
       ReportDrop(pos, skip_status);
@@ -56,16 +56,16 @@ bool VReader::SkipToPos(size_t pos) {
 }
 
 bool VReader::ReadRecord(Slice* record, std::string* scratch, int& head_size)
-{//日志回放的时候是单线程
+{
     scratch->clear();
     record->clear();
 
     if(buffer_.size() < kVHeaderMaxSize)
-    {//遇到buffer_剩的空间不够解析头部时
+    {
         if(!eof_)
         {
             size_t left_head_size = buffer_.size();
-            if(left_head_size > 0)//如果读缓冲还剩内容，拷贝到读缓冲区头
+            if(left_head_size > 0)
                 memcpy(backing_store_, buffer_.data(), left_head_size);
             buffer_.clear();
             Status status = file_->Read(kBlockSize - left_head_size, &buffer_, backing_store_ + left_head_size);
@@ -80,10 +80,10 @@ bool VReader::ReadRecord(Slice* record, std::string* scratch, int& head_size)
                 eof_ = true;
                 return false;
             }
-            else if(buffer_.size() < kBlockSize)//因为前面回退了，所以这里是kblocksize
+            else if(buffer_.size() < kBlockSize)
             {
                 eof_ = true;
-                if(buffer_.size() < 4 + 1 + 1)//最少的一条记录也需要6个字节，一个字节的数据
+                if(buffer_.size() < 4 + 1 + 1)
                    return false;
             }
         }
@@ -96,9 +96,8 @@ bool VReader::ReadRecord(Slice* record, std::string* scratch, int& head_size)
             }
         }
     }
-    //解析头部
     uint64_t length = 0;
-    uint32_t expected_crc = crc32c::Unmask(DecodeFixed32(buffer_.data()));//早一点解析出crc,因为后面可能buffer_.data内容会变
+    uint32_t expected_crc = crc32c::Unmask(DecodeFixed32(buffer_.data()));
     buffer_.remove_prefix(4);
     const char *varint64_begin = buffer_.data();
     bool b = GetVarint64(&buffer_, &length);
@@ -106,7 +105,6 @@ bool VReader::ReadRecord(Slice* record, std::string* scratch, int& head_size)
     head_size = 4 + (buffer_.data() - varint64_begin);
     if(length <= buffer_.size())
     {
-        //逻辑记录完整的在buffer中(在block中)
         if (checksum_) {
             uint32_t actual_crc = crc32c::Value(buffer_.data(), length);
             if (actual_crc != expected_crc) {
@@ -122,15 +120,14 @@ bool VReader::ReadRecord(Slice* record, std::string* scratch, int& head_size)
     {
         if(eof_ == true)
         {
-            return false;//日志最后一条记录不完整的情况，直接忽略
+            return false;
         }
-        //逻辑记录不能在buffer中全部容纳，需要将读取结果写入到scratch
         scratch->reserve(length);
         size_t buffer_size = buffer_.size();
         scratch->assign(buffer_.data(), buffer_size);
         buffer_.clear();
         const uint64_t left_length = length - buffer_size;
-        if(left_length > kBlockSize/2)//如果剩余待读的记录超过block块的一半大小，则直接读到scratch中
+        if(left_length > kBlockSize/2)
         {
             Slice buffer;
             scratch->resize(length);
@@ -150,7 +147,7 @@ bool VReader::ReadRecord(Slice* record, std::string* scratch, int& head_size)
             }
         }
         else
-        {//否则读一整块到buffer中
+        {
             Status status = file_->Read(kBlockSize, &buffer_, backing_store_);
 
             if(!status.ok())
@@ -167,7 +164,7 @@ bool VReader::ReadRecord(Slice* record, std::string* scratch, int& head_size)
                     ReportCorruption(left_length, "last record not full");
                     return false;////////////////////////////////
                 }
-                eof_ = true; //这个判断不要也可以，加的话算是优化，提早知道到头了，省的read一次才知道
+                eof_ = true; 
             }
             scratch->append(buffer_.data(), left_length);
             buffer_.remove_prefix(left_length);
@@ -184,13 +181,8 @@ bool VReader::ReadRecord(Slice* record, std::string* scratch, int& head_size)
     }
 }
 
-//get查询中根据索引从vlog文件中读value值
 bool VReader::Read(char* val, size_t size, size_t pos)
-{/*//要考虑多线程情况*/
-    //MutexLock l(&mutex_);
-    //if (!SkipToPos(pos)) {//因为read读的位置随机，因此file的skip接口不行，因为file的skip是相对于当前位置的
-      //return false;
-    /*}*/
+{
     Slice buffer;
    // Status status = file_->Read(size, &buffer, val);
     Status status =  file_->Pread(pos, size, &buffer, val);
--- db/vlog_reader.h
+++ db/vlog_reader.h
@@ -27,27 +27,24 @@ class VReader {
     virtual void Corruption(size_t bytes, const Status& status) = 0;
   };
 
-  VReader(SequentialFile* file, bool checksum,
-         uint64_t initial_offset=0);//传入的file必须是new出来的
-  VReader(SequentialFile* file, Reporter* reporter, bool checksum,
-         uint64_t initial_offset=0);
+  VReader(SequentialFile* file, bool checksum, uint64_t initial_offset=0);
+  VReader(SequentialFile* file, Reporter* reporter, bool checksum, uint64_t initial_offset=0);
 
   ~VReader();
 
-  bool Read(char* val, size_t size, size_t pos);//从文件pos偏移读取size长的内容给val
-  //读取一条完整的日志记录到record，record的内容可能在scratch，也可能在backing_store_中
+  bool Read(char* val, size_t size, size_t pos);
   bool ReadRecord(Slice* record, std::string* scratch, int& head_size);
-  bool SkipToPos(size_t pos);//跳到文件指定偏移
-  bool DeallocateDiskSpace(uint64_t offset, size_t len);//释放offset偏移处len长的磁盘空间
+  bool SkipToPos(size_t pos);
+  bool DeallocateDiskSpace(uint64_t offset, size_t len);
 
  private:
 ////  port::Muex mutex_;
-  SequentialFile* const file_;//要读的文件
-  Reporter* const reporter_;//用于报告错误的
-  bool const checksum_;//是否进行数据校验
-  char* const backing_store_;//读缓冲区
-  Slice buffer_;//读缓冲区的封装，便于表示当前读缓冲区待读部分
-  bool eof_;   // Last Read() indicated EOF by returning < kBlockSize//是否读到文件尾了
+  SequentialFile* const file_;      // which file to read
+  Reporter* const reporter_;        // error reporter
+  bool const checksum_;             // should we do data checksum
+  char* const backing_store_;       // read buffer
+  Slice buffer_;
+  bool eof_;   // Last Read() indicated EOF by returning < kBlockSize
   // Reports dropped bytes to the reporter.
   // buffer_ must be updated to remove the dropped bytes prior to invocation.
   void ReportCorruption(uint64_t bytes, const char* reason);
